bacad4ac2e42aada1ce7e220d57911ba97916085
Shelsey Brinkerhoff <shelseyb@gmail.com>
2017-04-19 20:12:54 +0000

    added 'detail.html' and fixed bugs in urls.py

--- whatislife/urls.py
+++ whatislife/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<entry_title>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<entry_id>[0-9]+)/$', views.detail, name='detail'),
 ]
--- whatislife/views.py
+++ whatislife/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from .models import Post, Comments
+from django.template import loader
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Blog homepage")
+    latest_entry_list = Post.objects.order_by('-pub_date')[:5]
+    context = {'latest_entry_list': latest_entry_list}
+    return render(request, 'whatislife/index.html', context)
     
-def detail(request, entry_title):
-    return HttpResponse("You're looking at %s." % entry_title)
+def detail(request, entry_id):
+    try:
+        entry = Post.objects.get(pk=entry_id)
+    except entry.DoesNotExist:
+        raise Http404("Entry does not exist")
+    return render(request, 'whatislife/detail.html', {'entry': entry})
